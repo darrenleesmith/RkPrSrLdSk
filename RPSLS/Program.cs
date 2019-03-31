27e0adfb627d610d64c879c0ae604624575b140c
Darren Smith <darrensmith016@hotmail.com>
2019-03-31 21:31:46 +0100

    Loads...
    Changed Score, to an Array then Sum of Array rather than adding blindy to Score suring every move.
    
    Used PrintColourMessage on Start Text
    
    Removed DEBUG messages
    
    Computer Result changed to Cyan
    User Result changed to Green
    
    Score removed from Title and replaced with Username
    
    After each go, UserAttempt gets reset to zero in "retryMess" to avoid duplication.

--- RPSLS/Program.cs
+++ RPSLS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace RPSLS
 {
@@ -12,7 +13,6 @@ namespace RPSLS
             int Win = 0;
             int Lose = 0;
             int Draw = 0;
-            int Plays = 0;
 
             Console.Title = t.Rock + " " + t.Paper + " " + t.Scissors + " " + t.Lizard + " " + t.Spock + ": The Game by Darren Smith";
             Console.Write("What is your name? ");
@@ -21,18 +21,19 @@ namespace RPSLS
 
             while (true)
             {
-                Console.Title = (t.Rock + " " + t.Paper + " " + t.Scissors + " " + t.Lizard + " " + t.Spock + ": The Game by Darren Smith - Score [Win " + Win + "] [Draw " + Draw + "] [Lose " + Lose + "]");
+                Console.Title = (t.Rock + " " + t.Paper + " " + t.Scissors + " " + t.Lizard + " " + t.Spock + ": The Game by Darren Smith - User: "+ userName);
                 Random random = new Random();
                 int ComputerAttempt = random.Next(1, 6);
 
                 while (s.UserAttempt <= 5)
                 {
                     Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("DEBUG: Score: [Win {0}] [Draw {1}] [Lose {2}] [Total {3}]",Win,Draw,Lose,Plays);
-                    Console.ResetColor();
-                    Console.WriteLine("\nWelcome to {0}, {1}, {2}, {3}, {4}\n", t.Rock, t.Paper, t.Scissors, t.Lizard, t.Spock);
-                    Console.WriteLine("{0}: {1}\n{2}: {3}\n{4}: {5}\n{6}: {7}\n{8}: {9}\n", t.SelOne, t.Rock, t.SelTwo, t.Paper, t.SelThree, t.Scissors, t.SelFour, t.Lizard, t.SelFive, t.Spock);
+                    int[] array1 = { Win, Lose, Draw };
+                    int Plays = array1.Sum();
+
+                    PrintColourMessage(ConsoleColor.Red, "Score: [Win "+Win+"] [Draw "+Draw+"] [Lose "+Lose +"] [Total "+Plays+"]");
+                    PrintColourMessage(ConsoleColor.Yellow, "Welcome to "+t.Rock+ ", "+t.Paper+ ", " + t.Scissors+ ", " + t.Lizard+ ", " + t.Spock);
+                    PrintColourMessage(ConsoleColor.White, t.SelOne +": "+ t.Rock +"\n"+ t.SelTwo + ": " + t.Paper +"\n"+ t.SelThree + ": " + t.Scissors +"\n"+ t.SelFour + ": " + t.Lizard+"\n" + t.SelFive + ": " + t.Spock);
 
                     Console.Write("Please make a selection ({0} to {1}): ", t.SelOne, t.SelFive);
                     string userInput = Console.ReadLine();
@@ -41,10 +42,9 @@ namespace RPSLS
                         continue;
                     }
                     s.UserAttempt = Int32.Parse(userInput);
-                    Console.WriteLine("");
 
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("DEBUG: Computer seletected: {0} - ", ComputerAttempt);
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("\nComputer seletected: {0} - ", ComputerAttempt);
 
                     if (ComputerAttempt == 1)
                     {
@@ -68,8 +68,8 @@ namespace RPSLS
                     }
                     Console.ResetColor();
 
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("DEBUG: {0} seletected: {1} - ",userName, s.UserAttempt);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("{0} seletected: {1} - ",userName, s.UserAttempt);
 
                     if (s.UserAttempt == 1)
                     {
@@ -110,35 +110,30 @@ namespace RPSLS
                     {
                         Console.Write("\n{0} smashes {0} but nothing really happens",t.Rock);
                         Draw = Draw + 1;
-                        Plays = Plays + 1;
                         scoreDrawFun();
                     }
                     if (ComputerAttempt == 2)
                     {
                         Console.Write("\n{0} covers {1}", t.Paper, t.Rock);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                     if (ComputerAttempt == 3)
                     {
                         Console.Write("\n{0} breaks {1}", t.Rock, t.Scissors);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                     if (ComputerAttempt == 4)
                     {
                         Console.Write("\n{0} squashes {1}", t.Rock, t.Lizard);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                     if (ComputerAttempt == 5)
                     {
                         Console.Write("\n{0} vaporises {1}", t.Spock, t.Rock);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                 }
@@ -148,35 +143,30 @@ namespace RPSLS
                     {
                         Console.Write("\n{0} covers {1}", t.Paper,t.Rock);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                     if (ComputerAttempt == 2)
                     {
                         Console.Write("\n{0} rubs against {0}", t.Paper);
                         Draw = Draw + 1;
-                        Plays = Plays + 1;
                         scoreDrawFun();
                     }
                     if (ComputerAttempt == 3)
                     {
                         Console.Write("\n{0} cut {1}", t.Scissors,t.Paper);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                     if (ComputerAttempt == 4)
                     {
                         Console.Write("\n{0} eats {1}", t.Lizard,t.Paper);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                     if (ComputerAttempt == 5)
                     {
                         Console.Write("\n{0} disproves {1}", t.Paper,t.Spock);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                 }
@@ -186,35 +176,30 @@ namespace RPSLS
                     {
                         Console.Write("\n{0} breaks {1}", t.Rock,t.Scissors);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                     if (ComputerAttempt == 2)
                     {
                         Console.Write("\n{0} cut {1}", t.Scissors,t.Paper);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                     if (ComputerAttempt == 3)
                     {
                         Console.Write("\n{0} just rub together creating sparks",t.Scissors);
                         Draw = Draw + 1;
-                        Plays = Plays + 1;
                         scoreDrawFun();
                     }
                     if (ComputerAttempt == 4)
                     {
                         Console.Write("\n{0} decapitates {1}", t.Scissors,t.Lizard);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                     if (ComputerAttempt == 5)
                     {
                         Console.Write("\n{0} smashes {1}", t.Spock,t.Scissors);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                 }
@@ -224,35 +209,30 @@ namespace RPSLS
                     {
                         Console.Write("\n{0} crushes {1}", t.Rock,t.Lizard);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                     if (ComputerAttempt == 2)
                     {
                         Console.Write("\n{0} eats {1}", t.Lizard,t.Paper);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                     if (ComputerAttempt == 3)
                     {
                         Console.Write("\n{0} decapitates {1}", t.Scissors,t.Lizard);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                     if (ComputerAttempt == 4)
                     {
                         Console.Write("\nThe {0} starts reproducing", t.Lizard);
                         Draw = Draw + 1;
-                        Plays = Plays + 1;
                         scoreDrawFun();
                     }
                     if (ComputerAttempt == 5)
                     {
                         Console.Write("\n{0} poisons {1}", t.Lizard,t.Spock);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                 }
@@ -262,33 +242,30 @@ namespace RPSLS
                     {
                         Console.Write("\n{0} vaporizes {1}", t.Spock,t.Rock);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                     if (ComputerAttempt == 2)
                     {
                         Console.Write("\n{0} disproves {1}", t.Paper,t.Spock);
                         Lose = Lose + 1;
-                        Plays = Plays + 1;
                         scoreLoseFun();
                     }
                     if (ComputerAttempt == 3)
                     {
                         Console.Write("\n{0} smashes {1}", t.Spock,t.Scissors);
                         Win = Win + 1;
-                        Plays = Plays + 1;
                         scoreWinFun();
                     }
                     if (ComputerAttempt == 4)
                     {
                         Console.Write("\n{0} poisons {1}", t.Lizard,t.Spock);
-                        Lose = Lose + 1; Plays = Plays + 1;
+                        Lose = Lose + 1;
                         scoreLoseFun();
                     }
                     if (ComputerAttempt == 5)
                     {
                         Console.Write("\nLive long and prosper");
-                        Draw = Draw + 1; Plays = Plays + 1;
+                        Draw = Draw + 1;
                         scoreDrawFun();
                     }
                 }
@@ -297,27 +274,23 @@ namespace RPSLS
         public static void scoreWinFun()
         {
             PrintColourMessage(ConsoleColor.Green, " Win");
-            Score s = new Score();
-            s.UserAttempt = 0;
             retryMess();
         }
         public static void scoreDrawFun()
         {
             PrintColourMessage(ConsoleColor.Yellow, " Draw");
-            Score s = new Score();
-            s.UserAttempt = 0;
             retryMess();
             
         }
         public static void scoreLoseFun()
         {
             PrintColourMessage(ConsoleColor.Red, " Lose");
-            Score s = new Score();
-            s.UserAttempt = 0;
             retryMess();
         }
         static void retryMess()
         {
+            Score s = new Score();
+            s.UserAttempt = 0;
             Console.WriteLine("Press any key to try again");
             Console.ReadKey();
         }
